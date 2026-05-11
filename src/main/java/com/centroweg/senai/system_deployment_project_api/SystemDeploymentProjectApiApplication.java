dfd6b77eeafd65c957f24a40063a7c7210e34b62
bruno_l_medeiros <brunoluismedeiros.08@gmail.com>
2026-05-11 16:17:10 -0300

    feat: Add Maven Wrapper and initial Spring Boot application setup

---
+++ src/main/java/com/centroweg/senai/system_deployment_project_api/SystemDeploymentProjectApiApplication.java
@@ -0,0 +1,13 @@
+package com.centroweg.senai.system_deployment_project_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemDeploymentProjectApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemDeploymentProjectApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/centroweg/senai/system_deployment_project_api/SystemDeploymentProjectApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.centroweg.senai.system_deployment_project_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemDeploymentProjectApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
